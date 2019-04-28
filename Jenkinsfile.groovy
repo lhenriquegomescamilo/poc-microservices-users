3d8b77bf8f6958c158e6a9edf8ef37cf81523ba5
lcamilo <lhenriquegomescamilo@gmail.com>
2019-04-27 23:08:23 -0300

    Ajustando jenktins file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,23 +8,49 @@ podTemplate(
                 containerTemplate(alwaysPullImage: true, args: 'cat', command: '/bin/sh -c', image: 'lachlanevenson/k8s-helm:v2.11.0', name: 'helm-container', ttyEnabled: true)
         ],
         volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
-    node('poc-microservices-users') {
+    node('poc-microservices') {
+        def IMAGE_POSFIX = ""
+
         def REPOSITORY
         def GIR_URL_REPOSITORY = 'git@github.com:lhenriquegomescamilo/poc-microservices-users.git'
         def DOCKER_IMAGE = "gateway_users"
         def DOCKER_IMAGE_VERSION = ""
         def MICROSERVICE_NAME = "users"
-
+        def KUBE_NAMEPSACE = ""
+        def ENVIRONMENT = "dev"
+        def GIT_BRANCH
         def REPO_HELM_NAME = "poc-microservice"
-        def HELM_SERVICE_CHARMUSEUM_URL = "http://helm-chartmuseum:8080"
+        def HELM_SERVICE_CHARMUSEUM_URL = "http://10.100.174.52:8080"
+        def HELM_DEPLOY_NAME
+        def HELM_CHART_NAME = "${REPO_HELM_NAME}/${MICROSERVICE_NAME}"
+        def NODE_PORT = "30002"
 
         stage('Checkout') {
             echo 'Iniciando clone do Repositorio'
-            REPOSITORY = git credentialsId: 'github', url: GIR_URL_REPOSITORY
+            REPOSITORY = checkout([$class: 'GitSCM', branches: [[name: '*/master'], [name: '*/dev']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github', url: GIR_URL_REPOSITORY]]])
             echo REPOSITORY.toString()
+            GIT_BRANCH = REPOSITORY.GIT_BRANCH
+            if (GIT_BRANCH.equals("origin/master")) {
+                KUBE_NAMEPSACE = "prod"
+                ENVIRONMENT = 'prod'
+                HELM_DEPLOY_NAME = ENVIRONMENT + "-" + MICROSERVICE_NAME
+                echo "Ambiente production"
+                echo "CURRENT BRANCH ${GIT_BRANCH}"
+            } else if (GIT_BRANCH.equals("origin/dev")) {
+                KUBE_NAMEPSACE = "development"
+                ENVIRONMENT = 'development'
+                HELM_DEPLOY_NAME = ENVIRONMENT + "-" + MICROSERVICE_NAME
+                IMAGE_POSFIX = "-RC"
+                NODE_PORT = "30011"
+                echo "Ambiente development"
+                echo "CURRENT BRANCH ${GIT_BRANCH}"
+            } else {
+                echo "Não existe pipeline para a branch ${GIT_BRANCH}"
+                exit 0
+            }
             sh "ls -ltra"
             DOCKER_IMAGE_VERSION = sh label: 'get version', returnStdout: true, script: 'sh read-package-json-version.sh'
-            DOCKER_IMAGE_VERSION = DOCKER_IMAGE_VERSION.trim()
+            DOCKER_IMAGE_VERSION = DOCKER_IMAGE_VERSION.trim() + IMAGE_POSFIX
             echo "EXIBINDO DOCKER IMAGE VERSION ${DOCKER_IMAGE_VERSION}"
 
         }
@@ -49,7 +75,12 @@ podTemplate(
                 sh "helm repo update"
                 sh "helm repo list"
                 sh "helm search ${REPO_HELM_NAME}"
-                sh "helm upgrade ${MICROSERVICE_NAME} ${REPO_HELM_NAME}/${MICROSERVICE_NAME} --set image.tag=${DOCKER_IMAGE_VERSION}"
+
+                try {
+                    sh "helm upgrade --namespace=${KUBE_NAMEPSACE} ${HELM_DEPLOY_NAME} --set image.tag=${DOCKER_IMAGE_VERSION} --set service.nodePort=${NODE_PORT} ./config/${MICROSERVICE_NAME}"
+                } catch (Exception e) {
+                    sh "helm install --namespace=${KUBE_NAMEPSACE} --name=${HELM_DEPLOY_NAME} --set image.tag=${DOCKER_IMAGE_VERSION} --set service.nodePort=${NODE_PORT} ./config/${MICROSERVICE_NAME}"
+                }
 
             }
         }
